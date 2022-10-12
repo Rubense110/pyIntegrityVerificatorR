4cd3859ffe6c5de92a66636f399132bfc2d95dcb
Rubense110 <84941508+Rubense110@users.noreply.github.com>
2022-10-12 13:37:02 +0200

    Revert "arreglos"

--- pyIV/server.py
+++ pyIV/server.py
@@ -1,4 +1,3 @@
-from distutils.core import setup
 import socketserver
 import hmac
 import hashlib
@@ -8,7 +7,35 @@ import os
 
 import conf
 
-clave = "123456"
+def proof(data):
+    key= "123456"
+    hash_old = data.split("|")[2].strip()
+    nonce = data.split("|")[1].strip()
+    msg = data.split("|")[0].strip()
+    hash_new = hmac.new(key.encode(),(msg+nonce).encode(), hashlib.sha256).hexdigest()
+
+    res = ()
+
+    if nonce not in nonces:
+        nonces.append(nonce)
+        if hash_old==hash_new:
+            res = (0, hash_old, hash_new, msg)
+        else:
+            res = (2, hash_old, hash_new, msg)
+    else:
+        res = (1, hash_old, hash_new, msg)
+
+    return res
+
+def log(message, display=False):
+    """
+    Devuelve por consola y escribe el log en el log file.
+    """
+    if display:
+        print(message)
+    with open(conf.LOGS, 'a') as f:
+        f.write("\n"+message)
+        f.close()
 
 class Handler_TCPServer(socketserver.BaseRequestHandler):
     """
@@ -19,59 +46,44 @@ class Handler_TCPServer(socketserver.BaseRequestHandler):
             cliente.
 
     """
-    key = clave
-    nonces = []
-    def __init__(self, request, client_address, server):
-        super().__init__(request, client_address, server)
-        self.nonces = self.loadNonces()
     def handle(self):
-        
+
         local_time = time.strftime("[%d/%m/%y %H:%M:%S]", time.localtime())
 
         #Cargamos el ultimo mensaje recibido al servidor
         self.data = self.request.recv(1024).strip().decode()
         #Comprobamos integridad del mensaje recibido
-        cond = self.proof()
+        cond = proof(self.data)
         print("{} sent:".format(self.client_address[0]))
-        print(self.nonces)
-        message = local_time +" ["+self.client_address[0]+"]"+ " [notice] " + cond[3]
-        self.log(message,True)
+        print(nonces)
+        if cond[0]==0:
 
-        if cond[0]==0: print("Integridad correcta\n",self.data,self.request.sendall("ACK from TCP Server".encode()),"\n")
-        elif cond[0]==1: print("Fallo replay\n",self.data,self.request.sendall("Rechazado, Replay detectado".encode()),"\n")
-        else:print("Fallo integridad: %s != %s " %(cond[1], cond[2]),self.request.sendall("Rechazado, MitM detectado".encode()),"\n")
-            
-            # Devolvemos el ACK al cliente, confirmando el la llegada del mensaje
-    def loadNonces(self) -> list:
-        self.nonces = []
-        if os.path.exists(conf.NONCE_SERV):
-            with open(conf.NONCE_SERV,"rb") as f:
-                self.nonces = pickle.load(f)
+            message = local_time +" ["+self.client_address[0]+"]"+ " [notice] " + cond[3]
+            log(message,True)
+            print("Integridad correcta")
+            print(self.data)
+        elif cond[0]==1:
 
-    def proof(self) -> tuple:
-        msg, nonce, hash_old = self.data.split("|")
+            message = local_time +" ["+self.client_address[0]+"]"+ " [error_rp] " + cond[3]
+            log(message,True)
+            print("Fallo reply")
+            print(self.data)
+        else:
 
-        hash_new =  hmac.new(self.key.encode(),(msg+nonce).encode(), hashlib.sha256).hexdigest()
+            message = local_time + " ["+self.client_address[0]+"]"+ " [error_int] " + cond[3]
+            log(message,True)
+            print("Fallo integridad: %s != %s " %(cond[1], cond[2]))
+        # Devolvemos el ACK al cliente, confirmando el la llegada del mensaje
+        self.request.sendall("ACK from TCP Server".encode())
+    
 
-        if nonce not in self.nonces:
-            self.nonces.append(nonce)
-            if hash_old==hash_new: res = (0, hash_old, hash_new, msg)
-            else: res = (2, hash_old, hash_new, msg)
-        else: res = (1, hash_old, hash_new, msg)
-        return res
-
-    def log(self,mensaje,display=False):
-        """
-        Devuelve por consola y escribe el log en el log file.
-        """
-        if display:
-            print(mensaje)
-        with open(conf.LOGS, 'a') as f:
-            f.write("\n"+mensaje)
-            f.close()
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
+    nonces = []
+    if os.path.exists(conf.NONCE_SERV):
+        with open(conf.NONCE_SERV,"rb") as f:
+            nonces = pickle.load(f)
 
     #  Instanciamos el servidor TCP, aplicando el socket correspondiente (ip,puerto)
     tcp_server = socketserver.TCPServer((HOST, PORT), Handler_TCPServer)
@@ -79,14 +91,17 @@ if __name__ == "__main__":
     # Activamos el servidor TCP.
     # Para abortar el servidor presionar Ctrl-C
     try:
-        print("Servidor activo (HOST: %s ,PORT: %d)\n" %(HOST, PORT)) 
+        print("Servidor activo (HOST: %s ,PORT: %d)" %(HOST, PORT)) 
         tcp_server.serve_forever()
 
     except Exception as e:
         print("Error de apertura servidor: ",e)
     
     finally:
-        print("Hemos llegado con: ",tcp_server.nonces)
+        print("Hemos llegado con: ",nonces)
         with open(conf.NONCE_SERV,"wb") as f:
-            pickle.dump(tcp_server.nonces,f)
+            pickle.dump(nonces,f)
             f.close()
+
+
+    
