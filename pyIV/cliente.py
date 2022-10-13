9ae4a9677b9840188e808233325b144fcf562ad5
Rubense110 <84941508+Rubense110@users.noreply.github.com>
2022-10-13 21:34:08 +0200

    modularizacion

--- pyIV/cliente.py
+++ pyIV/cliente.py
@@ -1,15 +1,9 @@
-from logging import exception
 import socket
 import hmac
 import hashlib
 import secrets
-import os
-import pickle
 
-import conf
-
-
-
+from verificador import verificador
 
 class Generador():
 
@@ -20,12 +14,8 @@ class Generador():
         self.clave = clave                                                                               # Clave simetrica de cifrado
         self.nonce = secrets.token_urlsafe()                                                             # Nonce unico del mensaje
         self.msg_hmac = hmac.new(clave.encode(),(msg+self.nonce).encode(), hashlib.sha256).hexdigest()   # Resumen MAC del (mensaje+nonce) + clave simetrica
-        self.nonces = []
-        self.loadNonces()
 
-
-    # Conexion al servidor
-    def connect(self):
+    def connect(self):    # Conexion al servidor
         tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             tcp_client.connect((host_ip, server_port))
@@ -36,54 +26,25 @@ class Generador():
         self.received = tcp_client.recv(1024)
         tcp_client.close()
 
-        print ("\nBytes Enviados:     {}".format(self.data)+ "\nBytes Recibidos: {}".format(self.received.decode()))
-        cond = self.proof()
-        if cond==0:
-            print("Integridad correcta")
-        elif cond==1:
-            print("Incidencia de tipo 'Reply' detectada")
-        else:
-            print("Incidencia de tipo 'Integridad' detectada")
-        
-        with open(conf.NONCE_CLNT,"wb") as f:
-            pickle.dump(self.nonces,f)
-            f.close()
+        print ("\nBytes Enviados:     {}".format(self.data)+ "\nBytes Recibidos:    {}".format(self.received.decode()))
+        print ("Server reply :     ", self.received.decode().split("|")[0])
+        verificador(self.received.decode())
 
-
-    def loadNonces(self):
-        if os.path.exists(conf.NONCE_CLNT):
-            with open(conf.NONCE_CLNT,"rb") as f:
-                self.nonces = pickle.load(f)
-
-    def proof(self) -> int:
-        msg, nonce, hash_old = self.received.decode().split("|")
-        hash_new =  hmac.new(self.clave.encode(),(msg+nonce).encode(), hashlib.sha256).hexdigest()
-
-        if nonce not in self.nonces:
-            self.nonces.append(nonce)
-            if hash_old==hash_new: res = 0
-            else: res = 2
-        else: res = 1
-        return res
-
-    # Metodo para envio normal       
+           
     def send(self):
         self.data = "|".join([msg,self.nonce,self.msg_hmac])
         self.connect()
-    # Metodo para ataque de MitM
-    def mitM(self,newmsg):
+    
+    def mitM(self,newmsg):     
         self.data = "|".join([newmsg,self.nonce,self.msg_hmac])
         self.connect()
-    # Metodo para ataque de reply
-    def replay(self,replays):
+    
+    def replay(self,replays):   
         self.data = "|".join([msg,self.nonce,self.msg_hmac])
         i=0
         while(i<replays):
             self.connect()
             i+=1
-
-        
-
 
 if __name__ == "__main__":
     key= "123456"
@@ -91,12 +52,9 @@ if __name__ == "__main__":
     msg = "16272727 17172772 20000"
     msg2= "16272728 17172772 2000000"
 
-    a1 = Generador(host_ip,server_port,msg,key)            # mensaje normal, realmente no es ningun ataque
+    a1 = Generador(host_ip,server_port,msg,key)            
     a2 = Generador(host_ip,server_port,msg,key)
-    a3 = Generador(host_ip,server_port,msg,key)
-
-    #print(a1.nonce, a2.nonce, a3.nonce)
-    #print(a1.msg_hmac,a2.msg_hmac,a3.msg_hmac)
+    a3 = Generador(host_ip,server_port,msg,key) 
 
     a1.send()                                       # mensaje normal, realmente no es ningun ataque
     a2.mitM(msg2)                                   # ataque MitM
--- pyIV/server.py
+++ pyIV/server.py
@@ -1,13 +1,11 @@
-from distutils.core import setup
 import socketserver
 import hmac
 import hashlib
 import time
-import pickle
-import os
 import secrets
 
 import conf
+from verificador import verificador
 
 class Handler_TCPServer(socketserver.BaseRequestHandler):
     """
@@ -18,103 +16,48 @@ class Handler_TCPServer(socketserver.BaseRequestHandler):
             cliente.
 
     """
-    key = "123456"
-    nonces = []
+
     def __init__(self, request, client_address, server):
         super().__init__(request, client_address, server)
-        try: 
-            self.loadNonces()                               # Cargamos nonces almacenados en el registro del servidor
-                        # Cargamos nonce a enviar al cliente
-        except Exception as e:
-            print("Error en carga Nonces")
             
-
-
 
     def handle(self):
         
         local_time = time.strftime("[%d/%m/%y %H:%M:%S]", time.localtime())
+
         self.nonce = secrets.token_urlsafe()
-        # Cargamos el ultimo mensaje recibido al servidor
-        self.data = self.request.recv(1024).strip().decode()
-        # Comprobamos integridad del mensaje recibido
-        cond = self.proof()
-        # Almacenamos los nonces recibidos hasta el momento
-        self.saveNonces()
-        print("\n{} sent:".format(self.client_address[0]))
-        message = local_time +" ["+self.client_address[0]+"]"+ " [notice] " + cond[3]
-
-        if cond[0]==0:
-            message = local_time +" ["+self.client_address[0]+"]"+ " [notice] " + cond[3] 
-            print("Integridad correcta\n",self.data)
-
-            # Devolvemos el ACK al cliente, confirmando el la llegada del mensaje
-            # Envío nonce al cliente
-            msg = "ACK from TCP Server"                         
-
-        elif cond[0]==1: 
-            message = local_time +" ["+self.client_address[0]+"]"+ " [rp_error] " + cond[3]
-            print("Fallo replay\n",self.data)
-
-            # Envío nonce al cliente
-            msg = "Rechazado, Replay detectado"
-
-        else:
-            message = local_time +" ["+self.client_address[0]+"]"+ " [int_error] " + cond[3]
-            print("Fallo integridad: %s != %s " %(cond[1], cond[2]))
-
-            # Envío nonce al cliente
-            msg = "Rechazado, MitM detectado"
-
-        # Hacemos resumen del mensaje y nonce enviado por el servidor para blindar su conexion
+        self.data = self.request.recv(1024).strip().decode()     # Cargamos el ultimo mensaje recibido al servidor
         
-        hash_new =  hmac.new(self.key.encode(),(msg+self.nonce).encode(), hashlib.sha256).hexdigest()
+        print("\n{} sent:".format(self.client_address[0]))
+        verificator = verificador(self.data, sv= True)          # Comprobamos integridad del mensaje recibido
+        message = local_time +" ["+self.client_address[0]+"]"+ verificator.logData + self.data.split("|")[0]
+        
+        msg= verificator.msgSv
+
+        hash_new =  hmac.new(verificator.key.encode(),(msg+self.nonce).encode(), hashlib.sha256).hexdigest() # Hacemos resumen del mensaje y nonce enviado por el servidor
         respuesta =  "|".join([msg,self.nonce,hash_new])
-        self.request.sendall(respuesta.encode())
-        self.log(message,True)   
+        self.request.sendall(respuesta.encode())    # Enviamos la respuesta del servidor
+        self.log(message,True)  
+        print("Server reply :",msg) 
     
-    def saveNonces(self):
-        with open(conf.NONCE_SERV,"wb") as f:
-                pickle.dump(self.nonces,f)
-                f.close()
-
-    def loadNonces(self):
-        if os.path.exists(conf.NONCE_SERV):
-            with open(conf.NONCE_SERV,"rb") as f:
-                self.nonces = pickle.load(f)
-
-    def proof(self) -> tuple:
-        msg, nonce, hash_old = self.data.split("|")
-        hash_new =  hmac.new(self.key.encode(),(msg+nonce).encode(), hashlib.sha256).hexdigest()
-
-        if nonce not in self.nonces:
-            self.nonces.append(nonce)
-            if hash_old==hash_new: res = (0, hash_old, hash_new, msg)
-            else: res = (2, hash_old, hash_new, msg)
-        else: res = (1, hash_old, hash_new, msg)
-        return res
-
     def log(self,mensaje,display=False):
         """
         Devuelve por consola y escribe el log en el log file.
         """
-        if display:
-            print(mensaje)
+        if display: print(mensaje)        
         with open(conf.LOGS, 'a') as f:
             f.write("\n"+mensaje)
             f.close()
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    handler_tcp = Handler_TCPServer
-    #  Instanciamos el servidor TCP, aplicando el socket correspondiente (ip,puerto)
-    tcp_server = socketserver.TCPServer((HOST, PORT), handler_tcp)
 
-    # Activamos el servidor TCP.
-    # Para abortar el servidor presionar Ctrl-C
+    HOST, PORT = "localhost", 9999
+    handler_tcp = Handler_TCPServer  
+    tcp_server = socketserver.TCPServer((HOST, PORT), handler_tcp) #  Instanciamos el servidor TCP, aplicando el socket correspondiente y nuestro handler
+
     try:
-        print("Servidor activo (HOST: %s ,PORT: %d)\n" %(HOST, PORT)) 
-        tcp_server.serve_forever()
+        print("Servidor activo (HOST: %s ,PORT: %d)\n" %(HOST, PORT))   # Activamos el servidor TCP.
+        tcp_server.serve_forever()                                      # Para abortar el servidor presionar Ctrl-C
 
     except Exception as e:
         print("Error de apertura servidor: ",e)
