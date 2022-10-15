d493d6f3240639c0e9fecab62acc1207da31fec7
pabgalace <pablogaac@gmail.com>
2022-10-15 23:57:52 +0200

    prev-def

--- pyIV/client.py
+++ pyIV/client.py
@@ -2,8 +2,6 @@ import socket
 import hmac
 import hashlib
 import secrets
-import os
-import time
 
 from verifier import Verifier
 
@@ -21,11 +19,13 @@ class Handler_TCPClient():
 
     def connect(self):    # Server connection
         tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
         try:
-            tcp_client.connect((host_ip, server_port))
+            tcp_client.connect((self.host, self.port))
         except:
-            print("Unreachable Server")
+            print("Unreachable server..")
             exit(0)
+
         tcp_client.sendall(self.data.encode())
         self.received = tcp_client.recv(1024)
         tcp_client.close()
@@ -49,62 +49,6 @@ class Handler_TCPClient():
             self.connect()
             i+=1
 
-if __name__ == "__main__":
-
-    key= "123456"
-    host_ip, server_port = "127.0.0.1", 9999
-
-    print("Realizar transferencia bancaria \n")
-    cuenta_org = input("Introduce cuenta origen (6 digitos) : ")
-    cuenta_dest = input("Introduce cuenta destino (6 digitos) : ")
-    cantidad = input("Introduce la cantidad a mandar: ")
-    msg = cuenta_org+" "+cuenta_dest+" "+cantidad
-    print("\n Transferencia: ", msg)
-    
-    print("\n Acciones a realizar: ")
-    print("[1] Envio de transferencia")
-    print("[2] Simulacion ataque MitM")
-    print("[3] Simulacion ataque de Replay")
-    case1 = int(input("Escoja opcion: "))
-
-    while case1 >3 or case1==0:
-        case1 = int(input("Debe escoger entre [1 | 2 | 3], escoja opcion: "))
-
-    if case1 == 1:
-        print("####### Realizando la transferencia... #######\n")
-
-        a1 = Handler_TCPClient(host_ip,server_port,msg,key)
-        try: 
-            a1.send()
-        except Exception as e:
-            os.system("taskkill /f /im  server.exe")
-
-    elif case1 == 2:
-        print("####### Simulacion ataque MitM #######\n")
-
-        print("Modificar el mensage original...\n")
-        print("Cuenta origen original: ",cuenta_org)
-        cuenta_org1 = input("Introduce cuenta origen (6 digitos) : ")
-        print("\nCuenta destino original: ",cuenta_dest)
-        cuenta_dest1 = input("Introduce cuenta destino (6 digitos) : ")
-        print("\nCantidad original: ", cantidad)
-        cantidad1 = input("Introduce la cantidad a mandar: ")
-        msg1 = cuenta_org1+" "+cuenta_dest1+" "+cantidad1
-        print("\n Transferencia modificada ^_^: ", msg1)
-        a2 = Handler_TCPClient(host_ip,server_port,msg,key)
-        try: 
-            a2.mitm(msg1)
-        except Exception as e:
-            os.system("taskkill /f /im  server.exe")
-    else:
-        print("####### Simulación ataque de Replay #######\n")
-        
-        reps = int(input("Introducir el numero de repeticiones del mensaje: "))
-        a3 = Handler_TCPClient(host_ip,server_port,msg,key)
-        try:
-            a3.replay(reps)
-        except Exception as e:
-            os.system("taskkill /f /im  server.exe")
 
     
 
--- pyIV/lanzador.py
+++ pyIV/lanzador.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 
 import conf
+from client import Handler_TCPClient
 
 if __name__ == "__main__":
 
@@ -28,14 +29,66 @@ if __name__ == "__main__":
             time.sleep(2)
 
             #Ejecutamos el cliente
-            exec(open(os.path.join(conf.PATH,"client.py")).read())
+#            exec(open(os.path.join(conf.PATH,"client.py")).read())
+            
+            print("Realizar transferencia bancaria \n")
+            cuenta_org = input("Introduce cuenta origen (6 digitos) : ")
+            cuenta_dest = input("Introduce cuenta destino (6 digitos) : ")
+            cantidad = input("Introduce la cantidad a mandar: ")
+            msg = cuenta_org+" "+cuenta_dest+" "+cantidad
+            print("\n Transferencia: ", msg)
+            
+            print("\n Acciones a realizar: ")
+            print("[1] Envio de transferencia")
+            print("[2] Simulacion ataque MitM")
+            print("[3] Simulacion ataque de Replay")
+            case1 = int(input("Escoja opcion: "))
 
-            print("Apagando el servidor...")
-            time.sleep(2)
-            os.system("taskkill /f /im  server.exe")
-            print("Saliendo...")
-            time.sleep(1.5)
-            exit(0)
+            while case1 >3 or case1==0:
+                case1 = int(input("Debe escoger entre [1 | 2 | 3], escoja opcion: "))
+
+            if case1 == 1:
+                print("####### Realizando la transferencia... #######\n")
+                a1 = Handler_TCPClient(host_ip,server_port,msg,key)
+                try:
+                    a1.send()
+                except:
+                    os.system("taskkill /f /im  server.exe")
+                
+            elif case1 == 2:
+                print("####### Simulacion ataque MitM #######\n")
+
+                print("Modificar el mensage original...\n")
+                print("Cuenta origen original: ",cuenta_org)
+                cuenta_org1 = input("Introduce cuenta origen (6 digitos) : ")
+                print("\nCuenta destino original: ",cuenta_dest)
+                cuenta_dest1 = input("Introduce cuenta destino (6 digitos) : ")
+                print("\nCantidad original: ", cantidad)
+                cantidad1 = input("Introduce la cantidad a mandar: ")
+                msg1 = cuenta_org1+" "+cuenta_dest1+" "+cantidad1
+                print("\n Transferencia modificada ^_^: ", msg1)
+                a2 = Handler_TCPClient(host_ip,server_port,msg,key)
+                try: 
+                    a2.mitm(msg1)
+                except Exception as e:
+                    os.system("taskkill /f /im  server.exe")
+            else:
+                print("####### Simulación ataque de Replay #######\n")
+                
+                reps = int(input("Introducir el numero de repeticiones del mensaje: "))
+                a3 = Handler_TCPClient(host_ip,server_port,msg,key)
+                try:
+                    a3.replay(reps)
+                except Exception as e:
+                    os.system("taskkill /f /im  server.exe")
+
+
+                print("Apagando el servidor...")
+                time.sleep(2)
+                os.system("taskkill /f /im  server.exe")
+                print("Saliendo...")
+                time.sleep(1.5)
+                exit(0)
         elif case == 2:
             print("Estableciendo periodo de auditoria..")
             print("Se realizan revisiones diarias")
